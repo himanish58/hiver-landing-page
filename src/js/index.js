3a2bdad057f8b17a91260ca2d6d6e2d3d221d150
himanish58 <himanish.kabiraj@gmail.com>
2017-10-17 03:50:55 +0530

    Started working on the navbar and quote

--- src/js/index.js
+++
@@ -1,9 +0,0 @@
-var btn = document.getElementsByTagName('button');
-btn[0].addEventListener('click', fun);
-function fun() {
-  alert('Button Clicked');
-}
-
-// let fun = () => {
-//   alert('Button Clicked');
-// };
